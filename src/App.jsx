40fc1d1c90d63b32a258ebf73f010f64bbf7c4a8
marielaure1 <edjour.marielaure@gmail.com>
2022-04-05 16:18:29 +0200

    Ajout Home et Result

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import './reset.css';
 import './App.scss';
 import { Home } from "./components/Home"
+// import { Results } from "./components/Result"
+
+// import { Routes, Route } from "react-router-dom"
 
 export const App = () => {
     return (
         <Home/>
+        // <Routes>
+            // <Route path="" element={<Home />}
+            // <Route path="result" element={<Result />}
+        // </Routes>
     );
 }
 
