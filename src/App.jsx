15ac9607014507a02dd3c47ffb9d478b4a9b1057
marielaure1 <edjour.marielaure@gmail.com>
2022-04-05 10:50:41 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './reset.css';
+import './App.scss';
+
+function App() {
+  return (
+    <>
+        <header>
+            <img src="images/Logo1.svg" alt=""/>
+            <h2>Définitions dans l’univers du Design sous toutes ses formes</h2>
+            <p className="choicePlanet">Choisi une planète</p>
+            <div className="planets">
+                <a href=""><img src="images/planet-pratique.svg" alt="" /></a>
+                <a href=""><img src="images/planet-impression.svg" alt="" /></a>
+                <a href=""><img src="images/planet-digital.svg" alt="" /></a>
+            </div>
+        </header>
+    </>
+  );
+}
+
+export default App;
