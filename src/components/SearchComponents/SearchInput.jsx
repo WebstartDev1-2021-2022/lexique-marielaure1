437be4abe2e39275f6edd533e3ea8915529534b6
marielaure1 <edjour.marielaure@gmail.com>
2022-05-31 15:49:16 +0200

    useDirectus

--- src/components/SearchComponents/SearchInput.jsx
+++ src/components/SearchComponents/SearchInput.jsx
@@ -1,32 +1,10 @@
 import { set } from "immutable"
 import { useState, useEffect} from "react"
 import { useNavigate } from 'react-router-dom'
+import { useDirectus } from "../../service/useDirectus"
 
 export const SearchInput = ({setPropositions}) => {
-    const [word, setWord] = useState('')
-    // const[responses, setResponses] = useState([])
-
-    useEffect(() => {
-
-        const  getDefinition = async () => {
-            if(!word.trim()){
-                setPropositions([])
-                return
-            }
-            try {
-                const response = await fetch(
-                    `https://umk1ukl6.directus.app/items/Definition?fields=name,id,category_id.name&filter[name][_starts_with]=${word}`)
-                const {data} = await response.json()
-
-                console.log(data)
-                setPropositions(data)
-            } catch(error){
-                console.error("SearchInput > getDefinition > error > ", error)
-            }
-        }
-        getDefinition()
-    }, [word])
-
+    const {word, setWord} = useDirectus(setPropositions)
     
     return (
         <form className="search-bar">
--- src/service/useDirectus.jsx
+++ src/service/useDirectus.jsx
@@ -1,14 +1,16 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
-export const useDirectus = ({setPropositions}) => {
-    
-    const [propositions, setPropositions] = useState([])
-    const [isLoading, setIsLoading] = useState(false)
-    const [error, setError] = useState(null)
+export const useDirectus = (setPropositions) => {
+    const [word, setWord] = useState("")
+    // const [propositions, setPropositions] = useState([])
+    // const [isLoading, setIsLoading] = useState(false)
+    // const [error, setError] = useState(null)
 
-    useEffect(() => {}, [word])
+    useEffect(()=> {
+        getPropositions()
+    }, [word])
 
-    const getPropositions = async (word) => {
+    const getPropositions = async () => {
         if(!word.trim()){
             setPropositions([])
             return
@@ -18,10 +20,12 @@ export const useDirectus = ({setPropositions}) => {
                 `https://umk1ukl6.directus.app/items/Definition?fields=name,id,category_id&filter[name][_starts_with]=${word}`)
             const {data} = await response.json()
 
-            console.log(data)
+            // console.log(data)
             setPropositions(data)
         } catch(error){
             console.error("SearchInput > getDefinition > error > ", error)
         }
     }
+
+    return {setWord, word}
 }
